a05d640e2f12c3e77f6a9fbac181fd2c08488d1d
Jordi Prats <jprats@systemadmin.es>
2016-06-03 17:42:04 +0200

    alineació

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,7 @@ class named::params {
   case $::osfamily
   {
     'redhat':
-        {
+    {
       case $::operatingsystemrelease
       {
         /^6.*$/:
@@ -27,7 +27,6 @@ class named::params {
         }
         default: { fail("Unsupported RHEL/CentOS version! - ${::operatingsystemrelease}")  }
       }
-
     }
     'Debian':
     {
